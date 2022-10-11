592fbc91ae41206d4176e001146cae016a490298
denis <106389581+fishman123456@users.noreply.github.com>
2022-10-11 16:51:34 +0700

    добавлены комментарии
    созданы массивы

--- Program.cs
+++ Program.cs
@@ -4,37 +4,25 @@
 // заполнить рандомными значениями от 1 до 100.
 // сделать  вывод массива на экран полностью и по индексу
 
-int[] myArr1;
-myArr1 = new int[20]; 
-Random rand = new Random();
-Console.WriteLine("Выберите способ заполнения: 1    2   3");
+int[] myArr_A; // обьявляем одномерный массив
+myArr_A = new int[5]; // задаём кол-во элементов
+float [,] myArr_B; // обьявляем одномерный массив
+myArr_B = new float[3,4]; // задаём кол-во элементов
+Random rand = new Random(); // выделяем память под функцию рандом
+Console.WriteLine(); // перенос на новую строку
+Console.WriteLine("Одномерный массив:\t"); // вывод на консоль 
 
-switch (Console.ReadKey().Key)
+for (int i = 1; i < myArr_A.Length; i++) 
 {
-    case ConsoleKey.NumPad1:
-            for (int i = 0; i < myArr1.Length; i++)
-            {
-                myArr1[i] = i;
-            }
-        print(myArr1);
-        break;
-        
-    case ConsoleKey.NumPad2:
-        break;
-    case ConsoleKey.NumPad3:
-        break;
-    case ConsoleKey.NumPad4:
-        break;
-        default:
-        Console.WriteLine("выход");
-        break;
-
-
-        static void print(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-        }
+    myArr_A[i] = i; // запоняем массив myArr_A
+}
+Console.WriteLine(); // перенос на новую строку
+print(myArr_A);     // функция вывода массива в одну строку
+Console.WriteLine(); // перенос на новую строку
+static void print(int[] arr) // функция вывода массива arr в одну строку
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " "); // вы
+    }
 }
